662030cc100d7f30b1e1efd224d16b145ce8998d
Despirado <leonard.monjok@gmail.com>
2026-06-25 19:37:12 +0100

    display todos

--- script.js
+++ script.js
@@ -1,18 +1,35 @@
-// --------------------------------------------------------------
+// ---------------------------------------------------- ----------
 // 1. ASYNC FUNCTION to fetch data from JSONPlaceholder
 // --------------------------------------------------------------asyn
 async function getTodos() {
- 
-    const statusElemet = document.getElementById("todos")
-    try {
-   const res = await fetch("https://jsonplaceholder.typicode.com/todos");
-   const data = await res.json();
-        console.log(data); (error);
-    } catch (error) {
-     console.log   
+  const statusElemet = document.getElementById("status");
+  const listEl = document.getElementById("todoList");
+  statusElemet.textContent = " fetching todos...";
+  listEl.innerHtml = "";
+
+  try {
+    const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+    if (!res.ok) throw new Error("unable to fetch data");
+    const data = await res.json();
+    const todos = data.slice(0, 10);
+    if (todos.length === 0) {
+      listEl.innerHTML = `
+   <li>No todos at the moment</li>
+      `;
     }
+    todos.forEach(function (todo) {
+      const li = document.createElement("li");
+      li.className = "todo-item";
+      li.textContent = todo.title;
+      listEl.appendChild(li);
+    });
+    statusElemets.textContent = "todos has been loaded successfully";
+  } catch (error) {
+    console.log(error);
+  }
 }
-getTodos()
+
+getTodos();
 // --------------------------------------------------------------
 // 6. DISPLAY FUNCTION - renders todos in the list
 // --------------------------------------------------------------
